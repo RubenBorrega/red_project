7f49031b2fbd12ef7af6caf268f6dd6f7af2a5fd
Ruben Borrega <ruben.borrega1@gmail.com>
2016-04-23 18:36:57 +0000

    Come back and finish PLEASE

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     def new
         #form where a user can fil out their own profile
         @user = User.find(params[:user_id])
-        @profile = @user.build_profile
+        @profile = Profile.new
         
     end
     
